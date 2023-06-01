dfc3b48cfdef1af1359a190a4c12d54515d09bd3
unknown <benmamiaferiel@gmail.com>
2023-06-01 18:10:54 +0100

    history room

--- app/src/main/java/com/example/leoni/Historique.java
+++ app/src/main/java/com/example/leoni/Historique.java
@@ -2,13 +2,59 @@ package com.example.leoni;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Historique extends AppCompatActivity {
+    ImageButton nav;
+    Button hLtn1,hLtn2,hLtn3,hLtn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historique);
+        nav=findViewById(R.id.navButton);
+        hLtn1=findViewById(R.id.historiqueButtonLtn1);
+        hLtn2=findViewById(R.id.historiqueButtonLtn2);
+        hLtn3=findViewById(R.id.historiqueButtonLtn3);
+        hLtn4=findViewById(R.id.historiqueButtonLtn4);
+        nav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(),navBar.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        hLtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HistoryLtn1.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+        hLtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HistoryLtn2.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        hLtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),HistoryLtn3.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/leoni/Register.java
+++ app/src/main/java/com/example/leoni/Register.java
@@ -13,6 +13,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,28 +25,39 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
-    EditText UserName , Password;
+    EditText userName , password;
     FirebaseAuth mAuth;
     Button SignUp;
     TextView Title  ;
+    ImageButton nav;
     ProgressBar ProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         mAuth = FirebaseAuth.getInstance();
-        UserName =findViewById(R.id.Username);
-        Password = findViewById(R.id.Password);
+        userName =findViewById(R.id.Username);
+        password = findViewById(R.id.Password);
         Title= findViewById(R.id.Leoni);
         SignUp = findViewById(R.id.signup);
         ProgressBar = findViewById(R.id.Loading);
+        nav =findViewById(R.id.navButton);
+        nav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(),navBar.class);
+                startActivity(intent);
+                finish();
+            }
+        });
         SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ProgressBar.setVisibility(View.VISIBLE);
                 String Email , Pass;
-                Email = String.valueOf(UserName.getText());
-                Pass = String.valueOf(Password.getText());
+                Email = String.valueOf(userName.getText());
+                Pass = String.valueOf(password.getText());
                 if (TextUtils.isEmpty(Email)){
                     Toast.makeText(Register.this, "enter you email",Toast.LENGTH_SHORT).show();
                     return;
